70c5ef3cfbddd36c21f1ba1e2ef85dc55ce472c2
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-07-30 11:46:44 +0200

    create repo

---
+++ src/main/java/com/example/user_microservice/ApplicationUserEntity.java
@@ -0,0 +1,12 @@
+package com.example.user_microservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ApplicationUserEntity {
+
+}
